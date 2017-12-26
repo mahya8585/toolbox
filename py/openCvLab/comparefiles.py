ea1c6dc5352f6589fe95339e195bffc4653fe4d6
maayaj37 <ishida.m@opst.co.jp>
2017-12-26 18:04:46 +0900

    複数枚比較処理実装(結果が怪しい要検証・・・）

--- py/openCvLab/comparefiles.py
+++ py/openCvLab/comparefiles.py
@@ -1,33 +1,48 @@
 import cv2
 import os
 
+# 複数枚比較するよー
+# 同じサイズの画像を比較する前提なので画像リサイズ処理は省くよー
+
 IMAGE_DIR = os.path.abspath(os.path.dirname(__file__)) + '/comparison/'
-# 画像サイズが一緒でない場合は画像サイズを合わせたほうがいい
-# IMAGE_SIZE = (993, 449)
 
 # 特徴量抽出処理の選定
-detector = cv2.AKAZE_create()
-# detector = cv2.ORB_create()
+detector = cv2.ORB_create()
 
 # 比較元の特徴量抽出(今回はグレースケールでの抽出とする)
 SOURCE_FILE_NAME = '1.png'
 source = cv2.imread(IMAGE_DIR + SOURCE_FILE_NAME, cv2.IMREAD_GRAYSCALE)
-# source = cv2.resize(source, IMAGE_SIZE)
 
 (source_kp, source_des) = detector.detectAndCompute(source, None)
 
-# 比較対象の設定
-TARGET_FILE_NAME = '2.png'
-target = cv2.imread(IMAGE_DIR + TARGET_FILE_NAME, cv2.IMREAD_GRAYSCALE)
-# target = cv2.resize(source, IMAGE_SIZE)
+# 画像ディレクトリ内のファイルをすべて比較し、一番類似度の高いものを決定する
+first_prize = {
+    'name': '',
+    'similarity': 0
+}
+matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
+images = os.listdir(IMAGE_DIR)
+for image in images:
+    if image == SOURCE_FILE_NAME or image == '__init__.py':
+        continue
 
-(target_kp, target_des) = detector.detectAndCompute(target, None)
+    # 比較対象の設定
+    target = cv2.imread(IMAGE_DIR + image, cv2.IMREAD_GRAYSCALE)
+    (target_kp, target_des) = detector.detectAndCompute(target, None)
 
-# 類似度確認
-matches = cv2.BFMatcher(cv2.NORM_HAMMING).match(source_des, target_des)
-dist = [m.distance for m in matches]
-ret = sum(dist) / len(dist)
+    # 類似度確認
+    matches = matcher.match(target_des, source_des)
+    matches = sorted(matches, key=lambda x: x.distance)
 
-# 距離を算出しているので、数値が小さいほど類似度が高いといえる
-print(dist)
-print('類似度：' + str(ret))
+    dist = [m.distance for m in matches]
+    ret = sum(dist) / len(dist)
+
+    # 距離を算出しているので、数値が小さいほど類似度が高いといえる
+    # print(dist)
+    print(image + ' ：' + str(ret))
+
+    if first_prize['name'] == '' or first_prize['similarity'] > ret:
+        first_prize['name'] = image
+        first_prize['similarity'] = ret
+
+print('一番類似しているグラフは' + first_prize['name'] + 'で、' + str(first_prize['similarity']) + 'でした。')
