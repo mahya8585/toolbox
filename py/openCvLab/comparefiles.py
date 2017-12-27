136df30d7b3ca88f9daa63372fbc27bb793240ca
maayaj37 <ishida.m@opst.co.jp>
2017-12-27 11:40:02 +0900

    ちょっと手直し

--- py/openCvLab/comparefiles.py
+++ py/openCvLab/comparefiles.py
@@ -4,45 +4,78 @@ import os
 # 複数枚比較するよー
 # 同じサイズの画像を比較する前提なので画像リサイズ処理は省くよー
 
-IMAGE_DIR = os.path.abspath(os.path.dirname(__file__)) + '/comparison/'
+def read_gray(file_path):
+    """
+    openCV読み込み(グレースケール)
+    :param file_path:
+    :param mode:
+    :return:
+    """
+    return cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
 
-# 特徴量抽出処理の選定
-detector = cv2.ORB_create()
+def detect_features(cv_image):
+    """
+    特徴量の検出
+    :param cv_image:
+    :return:
+    """
+    # 特徴量抽出処理の選定
+    detector = cv2.ORB_create()
+    return detector.detectAndCompute(cv_image, None)
 
-# 比較元の特徴量抽出(今回はグレースケールでの抽出とする)
-SOURCE_FILE_NAME = '1.png'
-source = cv2.imread(IMAGE_DIR + SOURCE_FILE_NAME, cv2.IMREAD_GRAYSCALE)
-
-(source_kp, source_des) = detector.detectAndCompute(source, None)
-
-# 画像ディレクトリ内のファイルをすべて比較し、一番類似度の高いものを決定する
-first_prize = {
-    'name': '',
-    'similarity': 0
-}
-matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
-images = os.listdir(IMAGE_DIR)
-for image in images:
-    if image == SOURCE_FILE_NAME or image == '__init__.py':
-        continue
-
-    # 比較対象の設定
-    target = cv2.imread(IMAGE_DIR + image, cv2.IMREAD_GRAYSCALE)
-    (target_kp, target_des) = detector.detectAndCompute(target, None)
-
-    # 類似度確認
+def extract_matches(target_des, source_des):
+    """
+    類似度検出
+    :param target_des:
+    :param source_des:
+    :return:
+    """
+    matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
     matches = matcher.match(target_des, source_des)
-    matches = sorted(matches, key=lambda x: x.distance)
 
-    dist = [m.distance for m in matches]
-    ret = sum(dist) / len(dist)
+    return sorted(matches, key=lambda x: x.distance)
 
-    # 距離を算出しているので、数値が小さいほど類似度が高いといえる
-    # print(dist)
-    print(image + ' ：' + str(ret))
+def compare_images(target_dir, source_name, source_des):
+    """
+    対象ファイル群の中から比較元画像に一番類似している画像を検出する
+    :param target_dir:
+    :param source_name:
+    :param source_des:
+    :return:
+    """
+    first_prize = {
+        'name': '',
+        'similarity': 0
+    }
 
-    if first_prize['name'] == '' or first_prize['similarity'] > ret:
-        first_prize['name'] = image
-        first_prize['similarity'] = ret
+    for image in os.listdir(target_dir):
+        if image == source_name or image == '__init__.py':
+            continue
 
-print('一番類似しているグラフは' + first_prize['name'] + 'で、' + str(first_prize['similarity']) + 'でした。')
+        # 比較対象の設定
+        (target_kp, target_des) = detect_features(read_gray(target_dir + image))
+
+        # 類似度確認
+        dist = [m.distance for m in extract_matches(target_des, source_des)]
+        ret = sum(dist) / len(dist)
+
+        # 距離を算出しているので、数値が小さいほど類似度が高いといえる
+        print(image + ' ：' + str(ret))
+        if first_prize['name'] == '' or first_prize['similarity'] > ret:
+            first_prize['name'] = image
+            first_prize['similarity'] = ret
+
+    return first_prize
+
+
+if __name__ == '__main__':
+    IMAGE_DIR = os.path.abspath(os.path.dirname(__file__)) + '/comparison/'
+
+    # 比較元の特徴量抽出(今回はグレースケールでの抽出とする)
+    SOURCE_FILE_NAME = '1.png'
+    (source_kp, source_des) = detect_features(read_gray(IMAGE_DIR + SOURCE_FILE_NAME))
+
+    # 画像ディレクトリ内のファイルをすべて比較し、一番類似度の高いものを決定する
+    first_prize = compare_images(IMAGE_DIR, SOURCE_FILE_NAME, source_des)
+
+    print('一番類似しているグラフは' + first_prize['name'] + 'で、' + str(first_prize['similarity']) + 'でした。')
