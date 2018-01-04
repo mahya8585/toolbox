f87d688c97be12df6ef3af59ef364a92045044f5
maaya ishida <huki.j37@gmail.com>
2018-01-04 18:14:25 +0900

    3系不安定すぎ問題のため2系にダウングレード

--- py/slackChannelList/run.py
+++ py/slackChannelList/run.py
@@ -1,29 +1,32 @@
+# -*- coding: UTF-8 -*-
 import json
+import sys
+
+sys.path.append('../Lib')
 import requests
-
-
-# チャネル一覧の取得
-channel_list_api = 'https://slack.com/api/channels.list?token=xoxxxxxxxxxxxxxxxxxxxxxxx&pretty=1'
+ 
+# get channel list
+channel_list_api = 'https://slack.com/api/channels.list?token=xoxp-2876607195-xxxxxx&pretty=1'
 result_channels = requests.get(channel_list_api).json()
-print(str(result_channels['channels']))
+print(result_channels['channels'])
 
-# slack表示文言の作成
-link_url_prefix = 'https://xxxxxxxxxx.slack.com/archives/'
+# create slack message
+link_url_prefix = 'https://xxxxxx.slack.com/archives/'
 message = '気になるchannelにJoinしてみましょう。\n'
 for channel in result_channels['channels']:
     if not channel['is_archived']:
         channel_name = str(channel['name'])
-        description = str(channel['purpose']['value'])
+        description = channel['purpose']['value'].encode('unicode-escape').decode('unicode-escape').encode('utf-8')
         channel_id = str(channel['id'])
 
         message = message + '<' + link_url_prefix + channel_id + ' | #' + channel_name + '>  :  ' + description + '\n'
 
-# slack通知
-incoming_url = 'https://hooks.slack.com/services/xxxx/xxxx/xxxxxxxxx'
+# send slack
+incoming_url = 'https://hooks.slack.com/services/xxxx/xxxx/xxxx'
 
 post_body = {
     'text': message,
-    'username': 'Pyladies Japan',
+    'username': 'channel keeper',
     'icon_emoji': ':dancers:',
     'channel': 'general'
 }
