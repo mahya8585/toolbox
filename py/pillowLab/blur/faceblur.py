04aef8e029336bdb409d0907b8448a9d27a813c4
maaya ishida <huki.j37@gmail.com>
2019-01-16 10:18:45 +0900

    プチリファクタリング

--- py/pillowLab/blur/faceblur.py
+++ py/pillowLab/blur/faceblur.py
@@ -34,16 +34,17 @@ def create_blur_img(src_img, face_rectangle, blur_ratio):
     return src_img
 
 
-def get_face_rectangle(img):
+def get_face_rectangle(img, key):
     """
     Azure Cognitive Service face APIを呼び出し、顔の位置を取得する
-    :param img:
+    :param img: 対象の画像
+    :param key: face api の subscription key
     :return:
     """
     url = 'https://japaneast.api.cognitive.microsoft.com/face/v1.0/detect'
     header = {
         'Content-Type': 'application/octet-stream',
-        'Ocp-Apim-Subscription-Key': 'xxxxx'
+        'Ocp-Apim-Subscription-Key': key
     }
 
     res = requests.post(url, headers=header, data=open(img, 'rb'))
@@ -57,12 +58,17 @@ def main():
     ディレクトリ内すべての画像に処理をします
     :return:
     """
+    # TODO この5つの値は各自自由に設定ください。よくわからない場合はFace APIのサブスクリプションkeyだけ変更してもらえれば動きます
     # 作業ベースディレクトリ
     base_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)))
     # モザイクをかけたい画像が入っているディレクトリ
     img_dir = os.path.join(base_dir, 'target')
     # モザイク加工済み画像が出力されるディレクトリ
     output_dir = os.path.join(base_dir, 'result')
+    # Face API サブスクリプションkey
+    face_key = 'xxxxxxxx'
+    # blur 効果のratio(float)
+    ratio = 15.0
 
     # モザイクをかけたい画像が入ってるディレクトリ内すべてのファイルに処理をします
     for image in os.listdir(img_dir):
@@ -74,12 +80,11 @@ def main():
         im = Image.open(img_path)
 
         # 顔位置の特定
-        rectangles = get_face_rectangle(img_path)
+        rectangles = get_face_rectangle(img_path, face_key)
 
         # blur処理(1枚に複数の顔があったら全部にblur処理かけるよ～）
         blur_img = im
         for rectangle in rectangles:
-            ratio = 15.0
             blur_img = create_blur_img(blur_img, rectangle['faceRectangle'], ratio)
 
         # ファイルの書き出し
