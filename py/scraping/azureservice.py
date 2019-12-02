4c06df6c8d5fc9305c8667ccbf5760c36ec6e394
maaya <huki.j37@gmail.com>
2019-12-02 14:35:04 +0900

    カテゴリ・サービス名の処理追加

--- py/scraping/azureservice.py
+++ py/scraping/azureservice.py
@@ -23,8 +23,33 @@ def extract_name(web_page):
         print(key)
 
 
+def extract_category_name(web_page):
+    """重複アリ・カテゴリと名前のセットを取得する
+    :param web_page: request.textデータ
+    """
+    # とりあえずhtml_parserで回すよー
+    page_soup = BeautifulSoup(web_page.text, 'html.parser')
+    contents = page_soup.find(id='products-list')
+    h2s = contents.find_all('h2')
+
+    category = ''
+    for h2 in h2s:
+        line = h2.find('span')
+
+        if line is None:
+            category = h2.text
+            continue
+        else:
+            print(category + ',' + line.text)
+
+
 # メイン処理ここから
-url = 'https://azure.microsoft.com/ja-jp/services/'
-extract_name(requests.get(url))
+url = requests.get('https://azure.microsoft.com/ja-jp/services/')
+
+# サービス名だけ(重複排除済み・プレビュー込み)
+extract_name(url)
+# カテゴリ・サービス名の対応一覧(サービス重複のまま)
+extract_category_name(url)
+
 
 print('function end.')
