6e57bb7cca20e234fb539465d52959540fff95e5
maaya <huki.j37@gmail.com>
2022-02-09 11:44:46 +0900

    ttmデータの登録処理

---
+++ py/ttmAPI/registerttm.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import requests
+import io
+import datetime
+import redis
+
+"""みずほのサイトからデータを取得し、DBに登録する
+:param year: 登録したい年
+"""
+# みずほのサイトからデータを取得
+FILE_PATH = 'https://www.mizuhobank.co.jp/market/csv/quote.csv'
+csv_file = requests.get(FILE_PATH)
+
+# # 日付とドルデータのみ抽出&便宜上表頭付与
+last_year = str(datetime.datetime.now().year - 1)
+df = pd.read_csv(io.BytesIO(csv_file.content), sep=',', encoding='shift-jis')
+usd_ttms = df.iloc[:, [0, 1]].dropna(axis=0).rename(columns={'Unnamed: 0': 'date', 'Unnamed: 1': 'usd'})
+
+# redis設定(既存データの削除をふくむ)
+redis_server = redis.Redis(
+    host='さーばほすと',
+    port=6380,
+    password='ぱすわーど',
+    ssl=True
+)
+redis_server.flushdb()
+
+# 日付が去年のデータをredisに登録
+for row in usd_ttms.itertuples():
+    if row.date.split('/')[0] == last_year:
+        redis_server.set(row.date, row.usd)
+
+print('function end.')
