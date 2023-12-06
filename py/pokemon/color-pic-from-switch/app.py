0491119034b2400cc540f56b9f9adcab0977a48d
maaya <huki.j37@gmail.com>
2023-12-06 13:53:53 +0900

    update

--- py/pokemon/color-pic-from-switch/app.py
+++ py/pokemon/color-pic-from-switch/app.py
@@ -6,7 +6,7 @@ import winsound
 import time
 
 
-# 取得したい色の範囲を指定・抽出 (今回は例としてズバットの色違いを指定)
+# 取得したい色の範囲を指定・抽出 (今回は例としてゴルバットの色違いを指定)
 poke_pic_color_lower = np.array([76, 115, 84])
 poke_pic_color_upper = np.array([114, 165, 86])
 
@@ -16,13 +16,18 @@ while True:
     im = ImageGrab.grab(all_screens=False)
 
     pic_color = cv2.inRange(np.array(im), poke_pic_color_lower, poke_pic_color_upper)
+    # 実際取れた値を画像化チェック
+    # im_ch = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
+    # extraction = cv2.bitwise_and(im_ch, im_ch, mask=pic_color)
+    # cv2.imwrite('target.jpg', extraction)
+
     found_it = False
     for line in pic_color:
         for pix in line:
             # 色の抽出ができていれば255が返ってくる
             if pix == 255:
                 # アラート音を鳴らす
-                winsound.PlaySound('DQ-levelup.mp3', winsound.SND_FILENAME)
+                winsound.PlaySound('DQ-levelup.wav', winsound.SND_FILENAME)
                 # macの人はこっち
                 # playsound('DQ-levelup.mp3')
                 found_it = True
@@ -33,21 +38,6 @@ while True:
             break
     
     # 停止時間を指定
-    time.sleep(10)
+    time.sleep(3)
 
-# pic_colorの中身チェック
-# lcnt = 0
-# for line in pic_color:
-#     pcnt = 0
-#         break
-#             # if pic > 0:
-#             #     print("line: " + str(lcnt) + " ◆pic: " + str(pic) + " ◆pcnt: " + str(pcnt))
 
-#             # pcnt += 1
-    
-#     lcnt += 1
-
-# 実際取れた値を画像化チェック
-# im_ch = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
-# extraction = cv2.bitwise_and(im_ch, im_ch, mask=pic_color)
-# cv2.imwrite('target.jpg', extraction)
